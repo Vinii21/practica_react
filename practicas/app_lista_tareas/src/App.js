f995d2f0e772b26af08973bdfd8d6743ba2a3e68
Vinicio Chavarría <vichavarria21@gmail.com>
2022-08-08 17:25:43 -0600

    Avance del proyecto: Componente Header.js, Componente FormularioTareas.js

--- practicas/app_lista_tareas/src/App.js
+++ practicas/app_lista_tareas/src/App.js
@@ -1,9 +1,32 @@
+import React, {useState} from 'react';
 import './App.css';
+import Header from './componentes/Header';
+import FormularioTareas from './componentes/FormularioTareas';
+
+
 
 const App = () => {
+  const [tareas, cambiarTareas] = useState(
+    [
+      {
+        id: 1,
+        texto: 'Lavar la ropa',
+        completada: false
+      },
+      {
+        id: 2,
+        texto: 'Leer',
+        completada: false
+      }
+    ]
+  )
+
+  console.log(tareas)
+
   return (
-    <div>
-      <h1>Hola Mundo</h1>
+    <div className="contenedor">
+      <Header />
+      <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas} />
     </div>
   );
 }
---
+++ practicas/app_lista_tareas/src/componentes/FormularioTareas.js
@@ -0,0 +1,47 @@
+import React, {useState} from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { v4 as uuidv4 } from "uuid";
+
+const FormularioTareas = ({tareas, cambiarTareas}) => {
+    const [inputTarea,cambiarEstadoInput] = useState('');
+
+    const handleInput = (e) => {
+        cambiarEstadoInput(e.target.value);
+    }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        cambiarTareas(
+            [
+                ...tareas,
+                 {
+                     id: uuidv4(),
+                     texto: inputTarea,
+                     completada: false
+                 }
+            ]
+        );
+    }
+
+    return (
+        <form action="" className="formulario-tareas" onSubmit={handleSubmit}>
+            <input
+            type="text"
+            className="formulario-tareas__input"
+            placeholder="Escribe una tarea"
+            value={inputTarea}
+            onChange={handleInput}
+            />
+            <button
+            type="submit"
+            className="formulario-tareas__btn"
+            >
+               <FontAwesomeIcon icon={faPlus} className="formulario-icon__btn" />     
+            </button>
+        </form>
+    );
+}
+ 
+export default FormularioTareas;
