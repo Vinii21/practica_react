601d5b0eff51ce6abe1db86945c8036473e30e22
Vinicio Chavarría <vichavarria21@gmail.com>
2022-08-09 22:15:09 -0600

    Logica en los componentes ListaTareas y Tarea

--- practicas/app_lista_tareas/src/componentes/Tarea.js
+++ practicas/app_lista_tareas/src/componentes/Tarea.js
@@ -2,12 +2,13 @@ import React, {useState} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckSquare, faSquare, faEdit, faTimes } from "@fortawesome/free-solid-svg-icons";
 
-const Tarea = ({tarea,toggleCompletada}) => {
+const Tarea = ({tarea,toggleCompletada,editarTarea,borrarTarea}) => {
     const [ editandoTarea, cambiarEditandoTarea ] = useState(false);
     const [ nuevaTarea, cambiarNuevaTarea ] = useState(tarea.texto);
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        editarTarea(tarea.id, nuevaTarea);
         cambiarEditandoTarea(false);
     }
 
@@ -38,6 +39,7 @@ const Tarea = ({tarea,toggleCompletada}) => {
                 /> 
                 <FontAwesomeIcon icon={faTimes} 
                 className="lista-tareas__icono lista-tareas__icono-accion"
+                onClick={()=> borrarTarea(tarea.id)}
                 /> 
             </div>
         </li>
